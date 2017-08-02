6074459d34d1ab1b2217bd961d0d166a655fb678
KACIMI\okacimi <okacimi@itemis.de>
2017-08-02 11:31:36 +0200

    refractored the login component

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -14,45 +14,38 @@ export class LoginComponent {
   static readonly CLIENT_ID = "131801329119-1v00skakag3jh7d3eev6va0gbr93lc11.apps.googleusercontent.com";
 
   gapiInstance: any;
-
-
   googleLoginButtonId = "google-login-button";
 
-  userAuthToken = null;
-  userDisplayName = "empty";
+  constructor(private _ngZone: NgZone, private user: User) {
+    this.gapiInstance = gapi;
+  }
 
   get getCLIENT_ID() {
     return LoginComponent.CLIENT_ID;
   }
 
   ngOnInit() {
+    this.initGoogleApi();
   }
 
-  constructor(private _ngZone: NgZone, private user: User) {
-    this.gapiInstance = gapi;
-    /*
-    this.getData();
-    */
-  }
-
-  // Angular hook that allows for interaction with elements inserted by the
-  // rendering of a view.
   ngAfterViewInit() {
     // Converts the Google login button stub to an actual button.
     this.drawSignInButton();
   }
 
+  initGoogleApi() {
+    this.gapiInstance.load('auth2',this.authenticateApp);
+  }
+
+  authenticateApp = () => {
+    var auth2 = gapi.auth2.init({
+      client_id: LoginComponent.CLIENT_ID,
+      hosted_domain: "itemis.de"
+    }
+    )
+  }
+
   drawSignInButton() {
-
-    this.gapiInstance.load('auth2', () => {
-      var auth2 = gapi.auth2.init({
-        client_id: LoginComponent.CLIENT_ID,
-        hosted_domain: "itemis.de"
-      }
-      ).then(() => {
-      })
-
-    });
     this.gapiInstance.signin2.render(
       this.googleLoginButtonId,
       {
@@ -73,34 +66,9 @@ export class LoginComponent {
   }
 
   logout() {
-    //You will be redirected to this URL after logging out from Google.
     let homeUrl = "http://localhost:4200";
     let logoutUrl = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=" + homeUrl;
     document.location.href = logoutUrl;
   }
-  /*
-    getData() {
-      gapi.load('auth2', () =>{
-        var auth2 = gapi.auth2.init({
-          client_id: LoginComponent.CLIENT_ID
-        }
-        ).then(() =>{
-          auth2 = gapi.auth2.getAuthInstance();
-          if (auth2.isSignedIn.get()) {
-            var profile = auth2.currentUser.get().getBasicProfile();
-            
-            console.log('ID: ' + profile.getId());
-            console.log('Full Name: ' + profile.getName());
-            console.log('Given Name: ' + profile.getGivenName());
-            console.log('Family Name: ' + profile.getFamilyName());
-            console.log('Image URL: ' + profile.getImageUrl());
-          }
-        })
-  
-      });
-  
-  
-    }
-  */
 
 }
