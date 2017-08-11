6293af3ed88b2e51a65603ddb1f83c8a4ca76600
KACIMI\okacimi <okacimi@itemis.de>
2017-08-11 14:26:43 +0200

    added the google logout as a precondition to e2e tests and finished implementing e2e

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,10 +1,13 @@
 import { browser, by, element } from 'protractor';
 
 export class TimeravelPage {
-  navigateTo() {
-    return browser.get('/');
-  }
 
+  private static readonly logoutUrl = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=" + "/";
+
+  navigateTo() {
+    return browser.driver.get(TimeravelPage.logoutUrl).then(()=>{browser.get('/')});
+  }
+  
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
